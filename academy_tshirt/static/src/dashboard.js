f33951e8ed6596191d4acdd2791f243556638ab8
boonruang <scubatoy@gmail.com>
2022-12-21 15:22:27 +0700

    2. Add some buttons for quick navigation

--- academy_tshirt/static/src/dashboard.js
+++ academy_tshirt/static/src/dashboard.js
@@ -3,6 +3,8 @@
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
 import { getDefaultConfig } from "@web/views/view";
+import { useService } from "@web/core/utils/hooks"
+import { Domain } from "@web/core/domain"
 
 const { Component,useSubEnv } = owl;
 
@@ -18,7 +20,39 @@ class AcademyDashboard extends Component {
         this.display = {
             controlPanel: { "top-right": false, "bottom-right" : false}
         };
+
+        this.action = useService("action");
+
     }
+
+    openCustomerView() {
+        this.action.doAction("base.action_partner_form");
+    }
+
+    openOrders(title,domain) {
+        this.action.doAction({
+            type: "ir.actions.act_window",
+            name: title,
+            res_model: "academy_tshirt_order",
+            domain: new Domain(domain).toList(),
+            views: [
+                [false, "list"],
+                [false, "form"]
+            ]
+        })
+    }
+
+    openLast7DaysOrders() {
+        const domain = 
+        "[('create_date', '>=', (context_today() - datetime.timedelta(days=7)).strftime('%Y-%m-%d'))]";
+        this.openOrders("Last 7 days orders", domain);
+    }
+
+    openLast7DaysCancelledOrders() {
+        const domain = 
+        "[('create_date', '>=', (context_today() - datetime.timedelta(days=7)).strftime('%Y-%m-%d')),('state', '=', 'cancelled')]";
+        this.openOrders("Last 7 days cancelled orders", domain);
+    }    
 }
 
 AcademyDashboard.components = { Layout };
